f46814749d79a508c5f8d090b4c57b0d66286255
zubayr1 <write2zubayr@gmail.com>
2022-02-19 17:48:35 +0100

    letssee6

--- src/components/Home_Dodo.js
+++ src/components/Home_Dodo.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef} from 'react'
+import HomeHeader from './HomeHeader';
 
 function Home_Dodo() {
+
+    const home = useRef(null)
+    const gallery = useRef(null)
+    const roadmap = useRef(null)
+  
+
+
+    const changeState = (childdata) => {
+    
+        if(childdata==='home')
+          home.current.scrollIntoView({ behavior: 'smooth' })
+        if(childdata==='gallery')
+          gallery.current.scrollIntoView({ behavior: 'smooth' }) 
+        if(childdata==='roadmap')
+          roadmap.current.scrollIntoView({ behavior: 'smooth' }) 
+      }
+
+
   return (
     <div>
-        ss
+        <HomeHeader changeState={changeState}/>
     </div>
   )
 }
