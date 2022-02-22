414f5d501ebf922081ac4978db5f3af4769fabfe
zubayr1 <write2zubayr@gmail.com>
2022-02-22 11:41:24 +0100

    scratch header

--- src/components/Scratchheader.js
+++ src/components/Scratchheader.js
@@ -10,19 +10,19 @@ import opensea from '../assets/opensea.png'
 function Scratchheader() {
   return (
     <div style={{ backgroundColor:'black'}}>
-    <div style={{marginLeft:'40px', marginRight:'40px',}}>
+    <div style={{marginLeft:'2%', marginRight:'2%',}}>
         
 
       <Grid  padded>
     
       <Grid.Row  verticalAlign="middle"> 
-        <Grid.Column floated='left'  mobile={6} tablet={8} computer={4}>
+        <Grid.Column floated='left'  mobile={5} tablet={6} computer={4}>
           <Link to="/">
             <Image src={logo} size='medium'/>
           </Link>
         </Grid.Column>
 
-        <Grid.Column floated='right' mobile={10} tablet={8} computer={4}>  
+        <Grid.Column floated='right' mobile={9} tablet={8} computer={4}>  
 
         <Grid>
             <Grid.Row columns={3} verticalAlign="middle">
