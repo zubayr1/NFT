9a96786df2bb7512a2d53221261de77a971cccd4
zubayr1 <write2zubayr@gmail.com>
2022-02-22 11:04:54 +0100

    header change

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -12,7 +12,7 @@ function HomeHeader({changeState}) {
 
 
   return (
-    <div style={{marginLeft:'40px', marginRight:'40px'}}>
+    <div style={{marginLeft:'2%', marginRight:'2%'}}>
         
 
         <Grid  padded>
