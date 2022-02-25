753897e5ecb726749b7656940ad37c0985da815a
zubayr1 <write2zubayr@gmail.com>
2022-02-25 18:06:40 +0100

    roadmap fontsize

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -58,7 +58,7 @@ export default function Roadmap() {
             
             <h3 className="vertical-timeline-element-title">15%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               We pay our Debts back.
             </p>
           </VerticalTimelineElement>
@@ -74,7 +74,7 @@ export default function Roadmap() {
           >
             <h3 className="vertical-timeline-element-title">30%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               You can create your own design. If the editorial team likes it, it will be auctioned in Drunk Dodo page and will be promoted via all Drunk Dodo social media. You keep 90% royalty if your NFT gets sold.
 
             </p>
@@ -92,7 +92,7 @@ export default function Roadmap() {
           >
             <h3 className="vertical-timeline-element-title">45%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               Customized Tees/Caps/Hoodies will be gifted to club members with pictures of their NFT Dodos printed on them. 
 
             </p>
@@ -119,7 +119,7 @@ export default function Roadmap() {
           >
             <h3 className="vertical-timeline-element-title">60%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               We will arrange our own online gaming events. Every members of our Discord channel are welcome to participate. The winners will be awarded with AR/VR Set, PS5/XBox Series X and NFTs. Remember, there will be multiple such events throughout our journey!
             </p>
 
@@ -144,7 +144,7 @@ export default function Roadmap() {
           >
             <h3 className="vertical-timeline-element-title">75%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               100,000$ will be donated to different NGOs working for Children Welfare and to Save Endangered Species like our favorite Dodos. It will be discussed with the most active members of the Club.
             </p>
           </VerticalTimelineElement>
@@ -161,7 +161,7 @@ export default function Roadmap() {
           >
             <h3 className="vertical-timeline-element-title">90%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               Live your wildest dreams! Attend the wildest drunk parties of Dodos in our base 'Dodos' Nest' in the Metaverse. Long nights of party and poker and lets bring Vegas in Metaverse!
 
             </p>
@@ -179,7 +179,7 @@ export default function Roadmap() {
           >
             <h3 className="vertical-timeline-element-title">100%</h3>
             
-            <p>
+            <p style={{fontSize:'14px'}}>
               Mythical performance in the Metaverse: Just imagine. There are flying mountains and sky kissing trees. A place populated by mythical creatures. And your favourite popstar is performing there in our unique place. Club members will be directly invited. Club members can apply for free passes to perform themselves! Its time to show your talent. In the Metaverse!
 
               
@@ -195,7 +195,7 @@ export default function Roadmap() {
             
           >
                         
-            <p>
+          <p style={{fontSize:'14px'}}>
             Oh! And one more Thing!- Drunk Dodos is a family, always: As the club grows, all the members will be invited to our next projects, and members will be collaborated for their own projects in the Metaverse. 
               Its time to rule Metaverse, as a family.
               
