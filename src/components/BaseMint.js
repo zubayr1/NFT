d4e8c79e9c4174e64076916c34d1edafdf6cfac2
zubayr1 <write2zubayr@gmail.com>
2022-05-05 15:13:43 +0200

    basemint price less

--- src/components/BaseMint.js
+++ src/components/BaseMint.js
@@ -39,7 +39,7 @@ function BaseMint(props) {
         }
         else if (check!==1 && defaultAccount!==null && to!=='' && amount!==0)
         {
-            var value = 0.002*amount;
+            var value = 0.0015*amount;
 
             
             
